afd1d79630d7f3ceec13a9fb664b735dbf9b36e6
sanjeevchoudhary1004 <sanjeevchoudhary1004@gmail.com>
2019-06-02 08:57:42 +0530

    Test for string methods

---
+++ stringmethods.py
@@ -0,0 +1,14 @@
+x = "lokeshit python"
+print(x)
+print(x.capitalize())
+print(x.title())
+print(x.upper())
+print(x.lower())
+print(x.isupper())
+print(x.islower())
+print(x.isalpha())
+print(x.isdigit())
+
+y = "12345"
+print(y)
+print(y.isdigit())
