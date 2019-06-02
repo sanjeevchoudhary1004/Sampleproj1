ff50b1919deb8061d0c3208bc86269f7e3a53f53
sanjeevchoudhary1004 <sanjeevchoudhary1004@gmail.com>
2019-06-02 10:28:02 +0530

    list operation

---
+++ listmethods1.py
@@ -0,0 +1,16 @@
+x=[10,20,30,40,50,20]
+print(x)
+for p in x:
+     print(p)
+i=0
+while i < len(x):
+    print(x[i])
+    i = i+1
+y = [1000,123.123,True]
+print(y)
+a,b,c = y
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+
+
