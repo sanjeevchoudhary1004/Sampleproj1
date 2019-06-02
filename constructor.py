58afbad062aca98093a5e39a08bb8c29c42aa1d5
sanjeevchoudhary1004 <sanjeevchoudhary1004@gmail.com>
2019-06-02 10:03:09 +0530

    constructor creation

---
+++ constructor.py
@@ -0,0 +1,16 @@
+class Test:
+    def __init__ (self):
+     print("in constructor of test")
+    def display(self):
+       print("Welcome")
+t1 = Test()
+t1.display()
+t2 = Test()
+t2.display()
+t2.display()
+
+
+
+
+
+
