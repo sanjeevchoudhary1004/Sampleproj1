b1295f0170c02ad31aeb3438d5aaa3607d6511f7
sanjeevchoudhary1004 <sanjeevchoudhary1004@gmail.com>
2019-06-02 09:32:37 +0530

    Operation on List

---
+++ listoperations.py
@@ -0,0 +1,21 @@
+x=[]
+print(x)
+print(type(x))
+print(len(x))
+
+y=list()
+print(y)
+print(type(y))
+print(len(y))
+
+z=[10,40,60,20,50,30]
+print(z)
+p=[100,200,300,100,200]
+print(p)
+
+r = [1000,123.123,True,3+4j]
+print(r)
+print(z[2])
+print(z[-2])
+z[3] = 70
+print(z)
